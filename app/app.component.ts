9a83752808a77f1c9f0bf55b16fb16ae71540f6b
mauridev <arismendimauricio@gmail.com>
2016-09-04 12:54:39 -0300

    Angular 2 Clean Start
    A starter point to my Angular 2 Apps

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Levantar entorno Angular2</h1><p>Poco a poco me estoy convirtiendo en un master chow de angular2</p>'
+})
+export class AppComponent { }
+
